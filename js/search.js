5cb0a431649f52a37ef6055b9ed131cddd5a2570
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-02-03 11:56:54 +0300

    four commit

--- js/search.js
+++ js/search.js
@@ -16,53 +16,69 @@ function search_ele() {
 //////////////// filtering
 $('.filter').change(function(){
 
-    filter_function();
-    
-    //calling filter function each select box value change
-    
-  });
+  filter_function();
   
-  $('.job_card .card .card-body ').show(); //intially all rows will be shown
+  //calling filter function each select box value change
   
-  function filter_function(){
-    $('.job_card .card .card-body ').hide(); //hide all rows
-    
-    var companyFlag = 0;
-    var companyValue = $('#filter-company').val();
-    var cityFlag = 0;
-    var cityValue = $('#filter-city').val();
- 
-    //setting intial values and flags needed
-    
-   //traversing each row one by one
-    $('.card .card-body').each(function() {  
-    
-      if(companyValue == 0){   //if no value then display row
-      companyFlag = 1;
-      }
-      else if(companyValue == $(this).find('h5.company').data('company')){ 
-        companyFlag = 1;       //if value is same display row
-      }
-      else{
-        companyFlag = 0;
-      }
-      
-      
-       if(cityValue == 0){
-      cityFlag = 1;
-      }
-      else if(cityValue == $(this).find('span.city').data('city')){
-        cityFlag = 1;
-      }
-      else{
-        cityFlag = 0;
-      }
+});
 
-     if(companyFlag && cityFlag){
-       $(this).show();  //displaying row which satisfies all conditions
-     }
+$('.job_card .card .card-body ').show(); //intially all rows will be shown
+
+function filter_function(){
+  $('.job_card .card .card-body ').hide(); //hide all rows
   
-  });   
-  }
-  ////////////////slide comp
+  var companyFlag = 0;
+  var companyValue = $('#filter-company').val();
+  var cityFlag = 0;
+  var cityValue = $('#filter-city').val();
+
+  //setting intial values and flags needed
   
+ //traversing each row one by one
+  $('.card .card-body').each(function() {  
+  
+    if(companyValue == 0){   //if no value then display row
+    companyFlag = 1;
+    }
+    else if(companyValue == $(this).find('h5.company').data('company')){ 
+      companyFlag = 1;       //if value is same display row
+    }
+    else{
+      companyFlag = 0;
+    }
+    
+    
+     if(cityValue == 0){
+    cityFlag = 1;
+    }
+    else if(cityValue == $(this).find('span.city').data('city')){
+      cityFlag = 1;
+    }
+    else{
+      cityFlag = 0;
+    }
+
+   if(companyFlag && cityFlag){
+     $(this).show();  //displaying row which satisfies all conditions
+   }
+
+});   
+}
+  ////////////////lang
+  function togglestyle() {
+    var styles = document.getElementsByTagName('link')[0];
+    var paragraphEN = document.getElementById('en');
+    var paragraphAR = document.getElementById('ar');
+
+    if (styles.getAttribute('href') == 'css/style.css') {
+        styles.setAttribute('href', 'css/styleEN.css');
+        paragraphEN.style.display = 'none';
+        paragraphAR.style.display = 'block';
+
+    }
+    else {
+        styles.setAttribute('href', 'css/style.css');
+        paragraphAR.style.display = 'none';
+        paragraphEN.style.display = 'block';
+    }
+}
